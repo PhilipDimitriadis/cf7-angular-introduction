66b20d80ac981683867df8b6d28307b3269b450e
Philip Dimitriadis <philipdimitriadis@gmail.com>
2025-05-12 18:35:07 +0300

    Updates

--- src/app/components/eperson-reactive-form/eperson-reactive-form.component.ts
+++ src/app/components/eperson-reactive-form/eperson-reactive-form.component.ts
@@ -24,7 +24,7 @@ import { EPerson } from 'src/app/shared/interfaces/eperson';
   styleUrl: './eperson-reactive-form.component.css'
 })
 export class EpersonReactiveFormComponent {
-    @Output() person = new EventEmitter<EPerson>()
+  @Output() person = new EventEmitter<EPerson>()
 
   form = new FormGroup({
     givenName: new FormControl('', Validators.required),
@@ -41,30 +41,27 @@ export class EpersonReactiveFormComponent {
 
   // form = new FormGroup<{
   //   givenName: FormControl<string>,
-  //   surName: FormControl<string>,
-  //   email: FormControl<string>,
-  //   age: FormControl<number>,
-  //   education: FormControl<string>
+  //   surName:FormControl<string>,
+  //   email:FormControl<string>,
+  //   age:FormControl<number>,
+  //   education:FormControl<string>
   // }>({
-  //   givenName: new FormControl('', {nonNullable: true, validators: Validators.required}),
-  //   surName: new FormControl('', {nonNullable: true, validators: Validators.required}),
-  //   age: new FormControl(18, {nonNullable: true,
-  //      validators: [
+  //   givenName: new FormControl('',{nonNullable:true, validators: Validators.required}),
+  //   surName: new FormControl('',{nonNullable:true, validators: Validators.required}),
+  //   age: new FormControl(18, { 
+  //     nonNullable:true, 
+  //     validators:[
   //       Validators.required, 
-  //       Validators.min(18), 
+  //       Validators.min(18),
   //       Validators.max(100)
   //     ]}),
-  //   email: new FormControl('', {nonNullable: true,
-  //      validators: [
-  //       Validators.required,
-  //        Validators.email
-  //       ]}),
-  //       education: new FormControl('', {nonNullable: true, validators: Validators.required})
+  //    email: new FormControl('', {nonNullable:true, validators:[Validators.required, Validators.email]}),
+  //    education: new FormControl('', {nonNullable: true, validators: Validators.required})
   // })
 
-  onSubmit() {
+  onSubmit(){
     if (this.form.valid) {
-      console.log(this.form.value)
+      // console.log(this.form.value);
 
       const person: EPerson = {
         givenName: this.form.value.givenName ?? '',
@@ -73,24 +70,23 @@ export class EpersonReactiveFormComponent {
         email: this.form.value.email ?? '',
         education: this.form.value.education ?? ''
       }
-      this.person.emit(person)
+      this.person.emit(person);
       this.form.reset()
     }
-    // console.log("Data", data)
-    // console.log(this.form)
-    // console.log("givenName", this.form.controls['givenName'].value)
-    // this.form.controls["surName"].setValue("Papakis")
+    // console.log("Data", data);
+    // console.log(this.form);
+    // console.log("givenName>>", this.form.controls['givenName'].value);
+    // this.form.controls["surName"].setValue("Papakis");
     // console.log(this.form.value)
   }
 
-  onSetValue() {
+  onSetValue(){
     this.form.setValue({
       givenName: "Kostas",
-      surName: "Lalakis",
+      surName:"Lalakis",
       age: 39,
-      email: "lalakis@aueb.gr",
-      education: "Bachelor's Degree"
+      email:"kostas@aueb.gr",
+      education: "Bachelor degree"
     })
   }
-
 }
--- src/app/components/simple-datatable/simple-datatable.component.ts
+++ src/app/components/simple-datatable/simple-datatable.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, Output, EventEmitter} from '@angular/core';
+import { Component, Input, Output, EventEmitter, effect, inject, SimpleChanges } from '@angular/core';
 import { EPerson } from 'src/app/shared/interfaces/eperson';
-import { sortBy} from 'lodash-es';
+import { sortBy } from 'lodash-es';
+// import { PersonService } from 'src/app/shared/services/person.service';
 
 @Component({
   selector: 'app-simple-datatable',
@@ -10,42 +11,70 @@ import { sortBy} from 'lodash-es';
 })
 export class SimpleDatatableComponent {
   @Input() data: EPerson[] | undefined;
+  @Input() myData: boolean = true;
   @Output() personClicked = new EventEmitter<EPerson>()
+
+  // personService = inject(PersonService)
+
+  epersonsData: EPerson[] = [];
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['data'] && this.data) {
+      console.log("ngOnChanges", this.data)
+      this.epersonsData = this.data
+    }
+    if (changes['myData']) {
+      console.log("myData")
+    }
+  }
+
+  // constructor(){
+  //   effect(()=>{
+  //     if(this.personService.modifiedDataTable()){
+  //       console.log("SIGNAL",this.data)
+  //       this.epersonsData = this.data
+  //     }
+  //     this.personService.modifiedDataTable.set(false);
+  //   })
+  // }
 
   sortOrder = {
     givenName: 'none',
     surName: 'none',
-    age: 'none',
-    email: 'none',
-    education: 'none'
+    age:'none',
+    email:'none',
+    education:'none'
   }
 
   sortData(sortKey: keyof EPerson): void {
-    console.log(sortKey);
-    if (this.sortOrder[sortKey]=== 'asc') {
-      this.sortOrder[sortKey] = 'desc'
-      this.data = sortBy(this.data, sortKey).reverse();
-    } else {
-      this.sortOrder[sortKey] = 'asc'
-      this.data = sortBy(this.data, sortKey);
-    }
+    // console.log(sortKey);
+    // this.epersonsData = this.data;
 
-    for (let key in this.sortOrder) {
-      if (key !== sortKey) {
+    if (this.sortOrder[sortKey]==='asc'){
+      this.sortOrder[sortKey] = 'desc'
+      this.epersonsData = sortBy(this.data, sortKey).reverse();
+    } else {
+      this.sortOrder[sortKey] = 'asc';
+      this.epersonsData = sortBy(this.data, sortKey);
+    }
+    
+    for (let key in this.sortOrder){
+      if (key!==sortKey) {
         this.sortOrder[key as keyof EPerson] = 'none'
       }
     }
-    console.log(this.sortOrder);
-  }
-  
-  onPersonClicked(person: EPerson) {
-    console.log("Person>>", person);
-    this.personClicked.emit(person);
+
+    console.log("Simple DataTable", this.data);
   }
 
   sortSign(sortKey: keyof EPerson): string {
-    if (this.sortOrder[sortKey] === 'asc') return '\u2191'
-    else if (this.sortOrder[sortKey] === 'desc') return '\u2193'
+    if (this.sortOrder[sortKey]==='asc') return '\u2191'
+    else if (this.sortOrder[sortKey]==='desc') return '\u2193'
     else return '';
   }
-} 
+
+  onPersonClicked(person:EPerson){
+    console.log("Person>>",person);
+    this.personClicked.emit(person);
+  }
+}
